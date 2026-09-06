3202e325ab53cb705270a5482d0c9528a74f97f1
Danila Vasilkov <106019215+MozzarellaCheesee@users.noreply.github.com>
2026-09-06 18:24:39 +0300

    style: remove navigationBarsPadding from bottom nav bar

--- app/src/main/kotlin/dev/nami/app/navigation/NamiNavHost.kt
+++ app/src/main/kotlin/dev/nami/app/navigation/NamiNavHost.kt
@@ -320,7 +320,7 @@ fun NamiNavHost(
                     }
                 }
             },
-            modifier = Modifier.navigationBarsPadding(),
+            modifier = Modifier,
         )
     }
 
