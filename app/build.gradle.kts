c887c4e4715082fe2a18c72a2e6b525fc6e0cc4f
Danila Vasilkov <106019215+MozzarellaCheesee@users.noreply.github.com>
2026-09-11 09:39:11 +0300

    chore(release): bump version to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = gitCommitCount
-        versionName = "0.1.0"
+        versionName = "1.0.0"
     }
     val keystoreFile = rootProject.file("secrets/nami.jks")
     val storePass = System.getenv("NAMI_KEYSTORE_PASSWORD")
